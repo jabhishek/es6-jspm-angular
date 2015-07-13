d21663775f7d7ab964ec6bf9adb228d455a85309
jabhishek <abhi2000@gmail.com>
2015-07-13 22:22:11 +0100

    Different package.json for client (jspm) and server (node)

--- client/config.js
+++ client/config.js
@@ -15,13 +15,13 @@ System.config({
 
 System.config({
   "map": {
-    "babel": "npm:babel-core@5.6.15",
-    "babel-runtime": "npm:babel-runtime@5.6.15",
+    "babel": "npm:babel-core@5.6.20",
+    "babel-runtime": "npm:babel-runtime@5.6.20",
     "core-js": "npm:core-js@0.9.18",
     "github:jspm/nodelibs-process@0.1.1": {
       "process": "npm:process@0.10.1"
     },
-    "npm:babel-runtime@5.6.15": {
+    "npm:babel-runtime@5.6.20": {
       "process": "github:jspm/nodelibs-process@0.1.1"
     },
     "npm:core-js@0.9.18": {
